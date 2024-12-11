54b412f27a0fb2b24e7a37f668cdc7cf7bbddc1e
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-12-11 12:45:44 +0530

    dotnet practise

---
+++ CsharpProjects/TestProject/Calculator.cs
@@ -0,0 +1,5 @@
+namespace CSharpFundamentals.Math{
+    public class Calculator{
+        public int Add(int a,int b) { return a + b; }
+    }
+}
---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,61 @@
+﻿// using CSharpFundamentals.Math;
+namespace CSharpFundamentals
+{
+//     public enum ShippingMethod{
+//         RegularAirMail = 1,
+//         RegisteredAirMail =2,
+//         Express = 3,
+
+//     }
+//        public class Person
+//     {
+//         public string firstName = "";
+//         public string lastName = "";
+//         public void Introduce()
+//         {
+//             Console.WriteLine("My name is " + firstName + " " + lastName);
+//         }
+//     }
+//        class Program {
+//         static void Main(string[] args){
+//             Person meena = new Person();
+//             Calculator calc = new Calculator();
+//             meena.firstName = "Meena";
+//             meena.lastName = "Krishna"; 
+//             meena.Introduce();
+//             // var result = calc.Add(1, 2);
+//             string name = string.Format("{0} {1}", meena.firstName, meena.lastName);
+//             Console.WriteLine("name:" + name);
+//             var numbers = new int[3] {1,2,3};
+//             string list = string.Join(" ", numbers);
+//             Console.WriteLine("list:" + list);
+//             var methodId = 2;
+//             Console.WriteLine((ShippingMethod)methodId);
+//             Console.WriteLine((int)ShippingMethod.RegularAirMail);
+//             var methodName = "Express";
+//             var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+//             Console.WriteLine(shippingMethod);
+//             var array1 = new int[3] { 1, 2, 3 };
+//             var array2 = array1;
+//             array2[0] = 0;
+//             Console.WriteLine(array2);
+
+//         }
+//     }
+class Program{
+    public static void Main(string[] args)  {
+        // var random = new Random();
+        // const int passowrdLength = 10;
+        //     var buffer = new char[passowrdLength];
+
+        // for(int i = 0; i < passowrdLength;i++){
+        //     buffer[i] = (char)('a'+random.Next(0,26));
+        // }
+
+        // var password = new string(buffer);
+        // Console.WriteLine(password);
+
+    }
+}
+}
+
