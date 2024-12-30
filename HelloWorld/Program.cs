45cb516fbbb993b5aaa28c8d36285fa4e8bddfee
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-12-30 12:25:23 +0530

    database operations

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Dapper;
+using HelloWorld.Data;
 using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 using System;
+using System.Collections;
 using System.Data;
 using System.Globalization;
 
@@ -12,13 +14,16 @@ namespace HelloWorld{
 
     internal class Program{
         public static void Main(string[] args) {
-            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
 
-            IDbConnection dbConnection = new SqlConnection(connectionString);
+            DataContextDapper dataContextDapper= new DataContextDapper();
+            // string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+            // IDbConnection dbConnection = new SqlConnection(connectionString);
 
             string sqlCommand = "SELECT GETDATE()";
+            DateTime rightNow = dataContextDapper.LoadDataSingle<DateTime>(sqlCommand);
 
-            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+            // // DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
 
             Console.WriteLine(rightNow.ToString());
 
@@ -26,27 +31,42 @@ namespace HelloWorld{
 
             Computer myComputer = new()
                 {
-                    MotherBoard ="Z076",
+                    MotherBoard ="Z075",
                     CPUCores = 3,
                     HasWifi = true,
                     HasLTE = false,
                         ReleaseDate = DateTime.Now,
-                        Price = 943.76m,
+                        Price = 950.76m,
                         VideoCard = "RTX 2060",
                 };
 
-string sql = @$"INSERT INTO TutorialAppSchema.Computer (
-            MotherBoard,CPUCores,HasWifi,HasLTE,ReleaseDate,Price,VideoCard) 
-            VALUES ('{ myComputer.MotherBoard}','{myComputer.CPUCores}' ,'{ myComputer.HasWifi}' ,'{myComputer.HasLTE}' ,'{myComputer.ReleaseDate.ToString("yyyy-MM-dd")}' ,'{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}','{myComputer.VideoCard}')";
+            string sql = @$"INSERT INTO TutorialAppSchema.Computer (
+                        MotherBoard,CPUCores,HasWifi,HasLTE,ReleaseDate,Price,VideoCard) 
+                        VALUES ('{ myComputer.MotherBoard}','{myComputer.CPUCores}' ,'{ myComputer.HasWifi}' ,'{myComputer.HasLTE}' ,'{myComputer.ReleaseDate.ToString("yyyy-MM-dd")}' ,'{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}','{myComputer.VideoCard}')";
 
-                Console.WriteLine(sql);
+            //                 Console.WriteLine(sql);
 
-            int result = dbConnection.Execute(sql);
+                        int result = dataContextDapper.ExecuteSqlWithRowCount(sql) ;
 
-            Console.WriteLine(result);
+                        Console.WriteLine(result);
             // Console.WriteLine(myComputer.MotherBoard);
             // Console.WriteLine(myComputer.HasWifi);
             // Console.WriteLine(myComputer.VideoCard);
+            string sqlSelect = @"SELECT 
+            Computer.MotherBoard,
+            Computer.CPUCores,
+            Computer.HasWifi,
+            Computer.HasLTE,
+            Computer.ReleaseDate,
+            Computer.Price,
+            Computer.VideoCard FROM TutorialAppSchema.Computer";
+
+             IEnumerable<Computer> computers = dataContextDapper.LoadData<Computer>(sqlSelect);
+            foreach(Computer singleComputer in computers){
+                Console.WriteLine($@"'{singleComputer.MotherBoard}','{singleComputer.HasWifi}','{singleComputer.HasLTE}','{singleComputer.ReleaseDate}','{singleComputer.Price}','{singleComputer.VideoCard}'");
+            }
         }
     }
+
+    
 }
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c4cd8ba8b8cfb9e25301785330e3226ea03b21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aaecbd1ff9405c60898a8b4179bb088dd45910b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
